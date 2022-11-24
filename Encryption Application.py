3677a5c7ab9a29cf24605682f8166aa542968989
celinewy <118931591+celinewy@users.noreply.github.com>
2022-11-23 19:01:33 -0700

    Update Encryption Application.py

--- Encryption Application.py
+++ Encryption Application.py
@@ -1,5 +1,5 @@
 # encryption.py
-# Celine Yeung, ENDG 233 F21
+# Celine Yeung
 # A terminal-based encryption application capable of both encoding and decoding text when given a specific cipher.
 # You may optionally import the string module from the standard Python library. No other modules may be imported.
 # Remember to include docstrings for your functions and comments throughout your code.
